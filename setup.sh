1647c5105d44069d96d8c031c5e79c26682167ed
Brendan Collins <brendan.m.collins@outlook.com>
2021-04-18 16:32:20 +0100

    git template

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@ ln -svfn ~/.dotfiles/.bashrc ~
 ln -svfn ~/.dotfiles/.zshrc ~
 sudo rm -rf ~/.vim
 ln -svfn ~/.dotfiles/.vim ~
+sudo rm -rf ~/.git_template
+ln -svfn ~/.dotfiles/.git_template ~
+chmod +x ~/.git_template/hooks/*
 mkdir -p ~/bin
 ln -svfn ~/.dotfiles/bin/custom_commands.sh ~/bin/
