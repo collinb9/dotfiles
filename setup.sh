06b1b092fb305b04122500d757055fb4ab4b8118
Brendan <brendan.m.collins@outlook.com>
2021-04-19 10:40:57 +0100

    zshrc update

--- setup.sh
+++ setup.sh
@@ -5,5 +5,8 @@ ln -svfn ~/.dotfiles/.vim ~
 sudo rm -rf ~/.git_template
 ln -svfn ~/.dotfiles/.git_template ~
 chmod +x ~/.git_template/hooks/*
+sudo rm -rf ~/.oh-my-zsh
+ln -svfn ~/.dotfiles/.oh-my-zsh ~
+chmod +x ~/.oh-my-zsh *
 mkdir -p ~/bin
 ln -svfn ~/.dotfiles/bin/custom_commands.sh ~/bin/
