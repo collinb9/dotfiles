d48731141bd3ab629b205f4807e090a3e1a04ca8
collinb9 <brendan.m.collins@outlook.com>
2021-10-20 15:25:57 +0200

    add .psqlrc

--- setup.sh
+++ setup.sh
@@ -1,21 +1,32 @@
-ln -svfn ~/.dotfiles/.zshrc ~
+# misc rc files
 ln -svfn ~/.dotfiles/.inputrc ~
-ln -svfn ~/.dotfiles/.gitconfig ~
 ln -svfn ~/.dotfiles/.screenrc ~
+ln -svfn ~/.dotfiles/.psqlrc ~
+
+# vim
 sudo rm -rf ~/.vim
 ln -svfn ~/.dotfiles/.vim ~
+
+# git
+ln -svfn ~/.dotfiles/.gitconfig ~
 sudo rm -rf ~/.git_template
 ln -svfn ~/.git_template ~
 chmod +x ~/.git_template/*
 git config --global init.templatedir '~/.git_template'
 ln -svfn ~/.dotfiles/.git_template ~
 chmod +x ~/.git_template/hooks/*
+
+# zsh
+ln -svfn ~/.dotfiles/.zshrc ~
 sudo rm -rf ~/.oh-my-zsh
 ln -svfn ~/.dotfiles/.oh-my-zsh ~
 chmod +x ~/.oh-my-zsh/*
+
+# fzf
 ln -svfn ~/.dotfiles/.fzf ~
 chmod +x ~/.fzf/*
 ~/.fzf/install
 chmod +x ~/.fzf/*
-# mkdir -p ~/bin
+
+# /bin
 ln -svfn ~/.dotfiles/bin/ ~
