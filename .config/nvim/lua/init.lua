5e53285d02d62b9cdb2f913179b309b58c5bfb5e
collinb9 <brendan.m.collins@outlook.com>
2021-12-03 10:59:07 +0100

    Add keybinfings for fugitive

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -19,4 +19,15 @@ if pcall(require, 'plenary') then
   end
 end
 
+-------- Key bindings
+
+-- Fugitive
+opts = {noremap = true, silent = true}
+vim.api.nvim_set_keymap('n', '<leader>gd', '<cmd>:G diff --cached<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>gc', '<cmd>:G commit -v<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>gp', '<cmd>:G push<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>gl', '<cmd>:G log<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>ga', '<cmd>:G add .<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>gu', '<cmd>:G add -u<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>gh', '<cmd>:G add %<CR>', opts)
 
