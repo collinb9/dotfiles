ad51d5add35b0a3f2f877bbed97530190c232461
collinb9 <brendan.m.collins@outlook.com>
2023-03-16 11:58:08 +0100

    Lighter virtualenv

--- bin/custom_commands.sh
+++ bin/custom_commands.sh
@@ -15,7 +15,7 @@ function virtualenv_venv(){
     python -m venv venv
     source venv/bin/activate
     python -m pip install --upgrade pip
-    python -m pip install black ipykernel pylint pip-tools jedi-language-server cfn-lint
+    python -m pip install black
     if [ -f "requirements.txt" ]; then
         python -m pip install -r requirements.txt
     fi
