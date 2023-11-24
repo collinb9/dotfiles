0a22f699b6b6939b55184f4d8622c0d1626c9bf2
collinb9 <brendan.m.collins@outlook.com>
2023-11-24 10:56:53 +0100

    Update opts to be closer to previous config

--- .config/nvim/lua/config/opts.lua
+++ .config/nvim/lua/config/opts.lua
@@ -1,9 +1,14 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.colorcolumn = "80"
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.expandtab = true
+
+-- Typewriter mode
+vim.opt.scrolloff = 999
 
 -- Tweaks for file browsing
 vim.g.netrw_banner=0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_localrmdir='rm -r'
-
--- vim.g.clipboard = 'unnamed'
